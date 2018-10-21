904843f2b48f2dc12686520d8fc252c77bc30388
Nick Shepherd <shepherdnick@hotmail.com>
2018-10-21 19:15:48 +0100

    Added unit testing project.

---
+++ TigerbayFlightApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace TigerbayFlightApp.Tests
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// I fully intended to write some unit tests for this project but simply ran out of time.
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
